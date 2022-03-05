7676431fc56f0e18c4b1c7eb19f6b4327f0d4667
Dominic3418 <101005648+Dominic3418@users.noreply.github.com>
2022-03-05 15:21:27 +0100

    Add files via upload

---
+++ Slider/js/main.js
@@ -0,0 +1,49 @@
+//swiper slider  
+      var swiper = new Swiper(".bg-slider-thumbs", {
+        loop: true,
+        spaceBetween: 0,
+        slidesPerView: 0,
+      });
+      var swiper2 = new Swiper(".bg-slider", {
+        loop: true,
+        spaceBetween: 0,
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+
+//navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+//Resoinsive navigation menu toggle
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () =>{
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
